6fe5b3b81161bd0b620bcbb8b8964934d1555e18
raphdf201 <raphdf201@hotmail.com>
2025-05-19 20:52:34 -0400

    Add docs

--- src/main/java/net/raphdf201/adventofcode2024/DayFour.java
+++ src/main/java/net/raphdf201/adventofcode2024/DayFour.java
@@ -5,11 +5,75 @@ import static net.raphdf201.adventofcode2024.Inputs.day4Xmas;
 public class DayFour {
     public static final String[] day4XmasArr = day4Xmas.split("\n");
 
+    /**
+     * <p>"Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the <a href="https://adventofcode.com/2019/day/10">Ceres monitoring station</a>!</p>
+     * <p>As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her <em>word search</em> (your puzzle input). She only has to find one word: <code>XMAS</code>.</p>
+     * <p>This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of <code>XMAS</code> - you need to find <em>all of them</em>. Here are a few ways <code>XMAS</code> might appear, where irrelevant characters have been replaced with <code>.</code>:<p>
+     * <pre><code>
+     * ..X...
+     * .SAMX.
+     * .A..A.
+     * XMAS.S
+     * .X....
+     * </code></pre>
+     * <p>The actual word search will be full of letters instead. For example:</p>
+     * <pre><code>
+     * MMMSXXMASM
+     * MSAMXMSMSA
+     * AMXSXMAAMM
+     * MSAMASMSMX
+     * XMASAMXAMM
+     * XXAMMXXAMA
+     * SMSMSASXSS
+     * SAXAMASAAA
+     * MAMMMXMMMM
+     * MXMXAXMASX
+     * </code></pre>
+     * <p>In this word search, <code>XMAS</code> occurs a total of <code><em>18</em></code> times; here's the same word search again, but where letters not involved in any <code>XMAS</code> have been replaced with <code>.</code>:</p>
+     * <pre><code>
+     * ....XXMAS.
+     * .SAMXMS...
+     * ...S..A...
+     * ..A.A.MS.X
+     * XMASAMX.MM
+     * X.....XA.A
+     * S.S.S.S.SS
+     * .A.A.A.A.A
+     * ..M.M.M.MM
+     * .X.X.XMASX
+     * </code></pre>
+     * <p>Take a look at the little Elf's word search. <strong>How many times does <code>XMAS</code> appear?</strong></p>
+     */
     public static void partOne() {
         System.out.print("4-1 : ");
         System.out.println(countOccurrences());
     }
 
+    /**
+     * <p>The Elf looks quizzically at you. Did you misunderstand the assignment?</p>
+     * <p>Looking for the instructions, you flip over the word search to find that this isn't actually an <code><em>XMAS</em></code> puzzle; it's an <code><em>X-MAS</em></code> puzzle in which you're supposed to find two <code>MAS</code> in the shape of an <code>X</code>. One way to achieve that is like this:</p>
+     * <pre><code>
+     * M.S
+     * .A.
+     * M.S
+     * </code></pre>
+     * <p>Irrelevant characters have again been replaced with <code>.</code> in the above diagram. Within the <code>X</code>, each <code>MAS</code> can be written forwards or backwards.</p>
+     * <p>Here's the same example from before, but this time all of the <code>X-MAS</code>es have been kept instead:</p>
+     * <pre><code>
+     * .M.S......
+     * ..A..MSMS.
+     * .M.S.MAA..
+     * ..A.ASMSM.
+     * .M.S.M....
+     * ..........
+     * S.S.S.S.S.
+     * .A.A.A.A..
+     * M.M.M.M.M.
+     * ..........
+     * </code></pre>
+     * <p>In this example, an <code>X-MAS</code> appears <code><em>9</em></code> times.</p>
+     * <p>Flip the word search from the instructions back over to the word search side and try again. <strong>How many times does an <code>X-MAS</code> appear?</strong></p>
+     */
     public static void partTwo() {
         System.out.print("4-2 : ");
         System.out.println(countXMASPatterns());
--- src/main/java/net/raphdf201/adventofcode2024/DayThree.java
+++ src/main/java/net/raphdf201/adventofcode2024/DayThree.java
@@ -6,6 +6,17 @@ import java.util.regex.Pattern;
 import static net.raphdf201.adventofcode2024.Inputs.day3Program;
 
 public class DayThree {
+    /**
+     * <p>"Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the <a href="https://adventofcode.com/2020/day/2">North Pole Toboggan Rental Shop</a>. The Historians head out to take a look.</p>
+     * <p>The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"</p>
+     * <p>The computer appears to be trying to run a program, but its memory (your puzzle input) is <em>corrupted</em>. All the instructions have been jumbled up!</p>
+     * <p>It seems like the goal of the program is just to <em>multiply some numbers</em>. It does that with instructions like <code>mul(X,Y)</code>, where <code>X</code> and <code>Y</code> are each 1-3 digit numbers. For instance, <code>mul(44,46)</code> multiplies <code>44</code> by <code>46</code> to get a result of <code>2024</code>. Similarly, <code>mul(123,4)</code> would multiply <code>123</code> by <code>4</code>.</p>
+     * <p>However, because the program's memory has been corrupted, there are also many invalid characters that should be <em>ignored</em>, even if they look like part of a <code>mul</code> instruction. Sequences like <code>mul(4*</code>, <code>mul(6,9!</code>, <code>?(12,34)</code>, or <code>mul ( 2 , 4 )</code> do <em>nothing</em>.</p>
+     * <p>For example, consider the following section of corrupted memory:</p>
+     * <pre>{@code xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))}</pre>
+     * <p>Only the four highlighted sections are real <code>mul</code> instructions. Adding up the result of each instruction produces <code><em>161</em></code> (<code>2*4 + 5*5 + 11*8 + 8*5</code>).</p>
+     * <p>Scan the corrupted memory for uncorrupted <code>mul</code> instructions. <strong>What do you get if you add up all the results of the multiplications?</strong></p>
+     */
     public static void partOne() {
         System.out.print("3-1 : ");
         String regex = "mul\\((\\d+),(\\d+)\\)";
@@ -22,6 +33,20 @@ public class DayThree {
         System.out.println(total);
     }
 
+    /**
+     * <p>As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.</p>
+     * <p>There are two new instructions you'll need to handle:</p>
+     * <ul>
+     * <li>The <code>do()</code> instruction <em>enables</em> future <code>mul</code> instructions.</li>
+     * <li>The <code>don't()</code> instruction <em>disables</em> future <code>mul</code> instructions.</li>
+     * </ul>
+     * <p>Only the <em>most recent</em> <code>do()</code> or <code>don't()</code> instruction applies. At the beginning of the program, <code>mul</code> instructions are <em>enabled</em>.</p>
+     * <p>For example:</p>
+     * <pre>{@code xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))}</pre>
+     * <p>This corrupted memory is similar to the example from before, but this time the <code>mul(5,5)</code> and <code>mul(11,8)</code> instructions are <em>disabled</em> because there is a <code>don't()</code> instruction before them. The other <code>mul</code> instructions function normally, including the one at the end that gets re-<em>enabled</em> by a <code>do()</code> instruction.</p>
+     * <p>This time, the sum of the results is <code><em>48</em></code> (<code>2*4 + 8*5</code>).</p>
+     * <p>Handle the new instructions; <strong>what do you get if you add up all the results of just the enabled multiplications?</strong></p>
+     */
     public static void partTwo() {
         System.out.print("3-2 : ");
         String regex = "mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)";
